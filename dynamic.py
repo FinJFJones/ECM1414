b0fcf31097d1a6ba01d335aeb4267289b5a3d298
FinJFJones <finley@finleyjones.co.uk>
2026-01-20 18:19:50 +0000

    Added approach ideas

--- dynamic.py
+++ dynamic.py
@@ -19,3 +19,16 @@ ACTIVITIES, NUM_ACTIVITIES, MAX_TIME, MAX_BUDGET = read_file('/Sample Input File
 
 assert NUM_ACTIVITIES == len(ACTIVITIES), 'Number of activities stated in file does not match the len of the activities list.'
 
+'''
+Approach:
+Activity 1 -> Activity 2 = Activity 2 -> Activity 1
+Therefore, timetable can be represented as a set as order doesn't matter
+
+As order is irrelevant, each activity is binary (either include or don't include)
+
+Activities where cost > budget can be discounted
+
+Activities where budget x = budget y but enjoyment x > enjoyment y can be discounted unless there is spare budget, but regardless x should always be preferred/tested first over y
+
+Activities can be ranked by enjoyment/budget. High ratios should be tested first.
+'''
