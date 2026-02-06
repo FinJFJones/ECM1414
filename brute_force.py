9e5882743084d54af167d2954e2115e29a0ec26e
jakebaxter00 <jb1860@exeter.ac.uk>
2026-02-06 17:20:38 +0000

    fix more spelling mistakes brute force

--- brute_force.py
+++ brute_force.py
@@ -7,13 +7,13 @@ This file contains algorithms for a brute force implementation of a budget alloc
 ## Group Packages ##
 from boiler_plates import ActivitySet
 
-def best_activity(activities, num_activites, current_activities, max_cost):
+def best_activity(activities, num_activities, current_activities, max_cost):
     '''
     A recursive solution to find the activities with the most enjoyment within the budget
     
     :param activities: List of activities
-    :param num_activites: Number of activities
-    :param current_activties: Array of current activties found
+    :param num_activities: Number of activities
+    :param current_activities: Array of current activities found
     :param max_cost: Budget constraint
     :return: Set of activities that match the result
     :rtype: ActivitySet
@@ -22,12 +22,12 @@ def best_activity(activities, num_activites, current_activities, max_cost):
     top_activity = ActivitySet(current_activities) #hold best activity, also initialised in case of all activities have been explored
 
     for activity in activities: #loop through all the activities so they can all be visited atleast once
-        if activity in current_activities: #we cannot repeat activites so skip these
+        if activity in current_activities: #we cannot repeat activities so skip these
             continue
         new_activity_set = ActivitySet(current_activities + [activity]) #use for checking constraint and passing recursively
         if new_activity_set.cost > max_cost:
             continue
-        activity_found = best_activity(activities, num_activites, new_activity_set.activities, max_cost) #recursively call algorithm and use the new activties
+        activity_found = best_activity(activities, num_activities, new_activity_set.activities, max_cost) #recursively call algorithm and use the new activities
         if activity_found.enjoyment > top_activity.enjoyment: #if better activity make it the best one with best enjoyment
             top_activity = activity_found
 
